629adb4d9ca407635ef4a3951e321a85d91a360a
xy1473210653 <xy17711043045@163.com>
2019-05-12 20:29:17 +0800

    设置butterknife

--- app/src/main/java/com/example/xianyang/mychat/MainActivity.java
+++ app/src/main/java/com/example/xianyang/mychat/MainActivity.java
@@ -2,11 +2,28 @@ package com.example.xianyang.mychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.common.app.Activity;
 
+import butterknife.BindView;
+
+public class MainActivity extends Activity {
+    @BindView(R.id.test_txt) TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getContentLayoutId() {
+
+        return R.layout.main_activity_layout;
+    }
+
+    @Override
+    protected void initwidget() {
+        super.initwidget();
+        textView.setText("你好啊啊啊啊啊");
     }
 }
